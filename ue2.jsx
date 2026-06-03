d4937a5f53c1be0ee9a7e499ad09cd3faecfed2c
khushidobariya1604 <khushidobariya1604@gmail.com>
2026-06-03 13:27:49 +0530

    Add files via upload

---
+++ ue2.jsx
@@ -0,0 +1,14 @@
+import { use, useEffect,useState} from "react";
+
+export default function UE2(){
+    const[date,setdate]=useState(new Date());
+
+    useEffect(()=>{setInterval(()=>{setdate(new Date())},1000)},[])
+
+    return(<>
+    <h1>{date.toLocaleTimeString()}</h1>
+    <h2>Hours : {date.getHours()}  Minutes : {date.getMinutes()}   seconds : {date.getSeconds()}</h2>
+
+    </>)
+}
+
